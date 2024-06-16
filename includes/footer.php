e9f4e0ade5595dd715faf49f1ab7cebfd78fafcd
jackhr <jc2o@mac.com>
2024-06-16 13:58:00 -0400

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,45 @@
+<footer>
+
+    <section id="contact-banner"></section>
+
+    <div class="inner">
+        <div>
+            <div class="footer-nav">
+                <h6>Navigation</h6>
+                <ul>
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/book-now.php">Book Now</a></li>
+                    <li><a href="/about.php">About</a></li>
+                    <li><a href="/faq.php">FAQ</a></li>
+                    <li><a href="/contact.php">Contact</a></li>
+                </ul>
+            </div>
+            <div class="footer-logo">
+                <div>
+                    <img src="/assets/images/logo.png" alt="Site Logo">
+                </div>
+            </div>
+            <div class="footer-contact">
+                <h6>Contact</h6>
+                <div class="contact-link">
+                    <span>Phone:</span>
+                    <a href="tel:+1 (268) 786-7559">+1 (268) 786-7559</a>
+                </div>
+                <div class="contact-link">
+                    <span>Email:</span>
+                    <a href="mailto:shaquanoneil99@gmail.com">shaquanoneil99@gmail.com</a>
+                </div>
+            </div>
+        </div>
+
+        <div id="copyright">© <?php echo date('Y'); ?> Shaquan's Car Rental Antigua   |   US$ to EC$ Exchange at 2.7   |   For discounted rate, enter 2 or more days.</div>
+    </div>
+
+</footer>
+
+UPDATE "GameWeek" set "startDate" = '2024-06-12 17:00:00' where id = 3;
+
+
+</body>
+
+</html>
